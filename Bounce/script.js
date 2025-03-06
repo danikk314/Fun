90ad172e69f5f602d82d4cddd2014b7baca70726
Danikk314 <da.bra3109@gmail.com>
2025-03-06 15:05:13 +0100

    -

---
+++ Bounce/script.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+canvas.width = 500 * 2;
+canvas.height = 500 * 2;
+canvas.style.width = "100%";
+canvas.style.height = "100%";
