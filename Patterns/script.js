de3fee648d5dd05dbbbbbb3766677141769631a4
danikk314 <da.bra3109@gmail.com>
2025-03-26 16:22:53 +0100

    26.03.25;16:22

--- Patterns/script.js
+++ Patterns/script.js
@@ -7,20 +7,34 @@ const p5 = [1, 14, 16, 17];
 const p = [p1, p2, p3, p4, p5];
 let Feld = [];
 
-
 function WasClicked(elem) {
     elem.style.backgroundColor = "#cc2222";
+    elem.classList.add("flipped");
+
+    
 
     let FID = parseInt(elem.id.match(/\d+/)[0], 10);
-    Feld.push(FID);
+    if (!Feld.includes(FID)) {
+        Feld.push(FID);
+    }
 
-    for (let j = 0; j < p.length;j++) {
-    for (let i = 0; i < p[j].length; i++) {
+    for (let j = 0; j < p.length; j++) {
+        for (let i = 0; i < p[j].length; i++) {
+            if (!(Feld.includes(p[j][i]))) {document.getElementById(`F${p[j][i]}`).style.backgroundColor = "aliceblue";}
+        }
+    }
 
+    for (let j = 0; j < p.length; j++) {
         if (Feld.every(item => p[j].includes(item))) {
-            document.getElementById(`F${p[j][i]}`).style.backgroundColor = "#cccc22";
-        };
-        
+            for (let i = 0; i < p[j].length; i++) {
+                if (!(Feld.includes(p[j][i]))) {document.getElementById(`F${p[j][i]}`).style.backgroundColor = "#cccc22";}
+            }
+        }
     }
-    }
+
+    setTimeout(() => {
+        for (let i = 0; i < Feld.length; i++) {
+            document.getElementById(`F${Feld[i]}`).style.backgroundColor = "#cc2222";
+        }
+    }, 250);
 }
